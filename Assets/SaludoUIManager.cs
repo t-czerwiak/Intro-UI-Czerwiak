1e90fe45cd8fc3f3a4afc04a74f6f2fb23890d5d
T-CZERWIAK <157724722+t-czerwiak@users.noreply.github.com>
2025-05-06 12:14:00 -0300

    terminado

---
+++ Assets/SaludoUIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SaludoUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtSaludo;
+    public TMP_InputField inputNombre;
+    string nombreIngresado;
+    void Start()
+    {
+        txtSaludo.text = "";
+           
+    }
+
+    public void Saludar()
+    {
+        txtSaludo.text = "Hola " + inputNombre.text + "!";
+    }
+
+}
