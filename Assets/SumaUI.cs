8423508468ca49b4cf56322e1c2bf6d617aa96bc
T-CZERWIAK <157724722+t-czerwiak@users.noreply.github.com>
2025-05-09 09:05:25 -0300

    terminado sumar y mostrar ui codigo

---
+++ Assets/SumaUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SumaUI : MonoBehaviour
+{
+    public TextMeshProUGUI txtResultado;
+    public TMP_InputField inputNum1;
+    public TMP_InputField inputNum2;
+    int num1;
+    int num2;
+
+    void Start()
+    {
+        txtResultado.text = "";
+    }
+
+    public void SumarYMostrar()
+    {
+        num1 = int.Parse(inputNum1.text);
+        num2 = int.Parse(inputNum2.text);
+        txtResultado.text = (num1 + num2).ToString();
+    }
+}
